7f8511231386b754d38c5c99c3a20f98e3a87653
harish836 <harish.mittal@hotmail.com>
2026-03-10 20:38:55 +0530

    Added new file
    Added new file

---
+++ Harish_New.py
@@ -0,0 +1,31 @@
+
+
+#This is first class content that I studied
+print("Hello World")
+print ("My name is Harish"),  print ("I am learning Python")
+print (100), print (3.14), print (2+3), #Python does the maths as well
+print ("I am" ,12 ,"years old")
+
+# This is a comment - Python ignores this line print("Comments help explain your code")
+# Inline comment # you can use comments to disable code temporarily # print("This line will not run")
+
+
+"""
+below is the exercise for the first class of day 1 learning Python,
+I am solving 4 questions here
+"""
+#First question is to write a program to print your school name and class
+print ("my name is harish and I study in 8th grade")
+
+#Print the number 1,2,3 each on a separate line using three print () statements.
+print (1)
+print (2)
+print (3)
+
+#what is the output of : print(5+3*2)?
+print (5+4*3)
+
+#print your full name , age and favourite subject using single print statement
+
+print("Harish Mittal",47,"Maths")
+
